cd4c4d64883303221e88cb6bae4860bde56d5a65
jleewebdev <lee45276@gmail.com>
2016-03-09 17:00:49 +0800

    Added tweeet button

--- javascripts/quote_machine.js
+++ javascripts/quote_machine.js
@@ -11,8 +11,20 @@ $(function() {
     })
     .done(function(data) {
       fillInjoke(data);
+      changeTwiterText(data);
       console.log("success");
     });
+  }
+
+  https://twitter.com/intent/tweet?text=Chuck%Norris%doesn%t%bowl%strikes%%he%just%knocks%down%one%pin%and%the%other%nine%faint
+
+  function changeTwiterText(res) {
+    console.log("cahnge")
+    var link_text = "https://twitter.com/intent/tweet?text=",
+        joke_text = res.value.joke.trim().replace(/\W/g, "%20"),
+        joke_text = joke_text.substring(0, 140);
+
+    $("a").attr("href", link_text + joke_text);
   }
 
   $("#fetch_quote").on("click", function() {
